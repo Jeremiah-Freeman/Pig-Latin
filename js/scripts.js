59825660497fcc2d6ec8e3e057ac70d58a8e9883
jeremiah freeman and nick wise <nick@>
2017-01-18 10:41:04 -0800

    create README,styles.css,scripts.js,index.html

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+
+
+
+
+
+
+$(document).ready(function(event){
+  $("form#Pig-Latin").submit(function(event) {
+    event.preventDefault();
+    
+
+  }
+
+
+
+});
