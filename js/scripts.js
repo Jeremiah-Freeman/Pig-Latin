d46eef6ea2e03e506d7a42400540c30945f10490
jeremiah freeman and nick wise <jeremiah@>
2017-01-18 16:48:38 -0800

    update,styles.css,scripts.js,index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,15 @@
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
-
-
-
-
-
-
 $(document).ready(function(event){
-  $("form#Pig-Latin").submit(function(event) {
+  $("form#pig-latin").submit(function(event) {
     event.preventDefault();
-    
 
-  }
-
-
-
+    var vowels = "a";
+    var userInput = $("input#translate").val();
+    var word = userInput.split(" ");
+    alert(word)
+    if (userInput === vowels) {
+      $("#result").show();
+    } else {
+      $("#result1").show();
+    }
+  });
 });
